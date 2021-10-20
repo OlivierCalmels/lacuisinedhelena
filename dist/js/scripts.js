66d648abca60e6bf3a3c8df6a4f8b9b92482de11
olivier calmels <calmels.o@gmail.com>
2021-10-20 21:31:31 +0200

    WIP

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-* Start Bootstrap - Business Casual v7.0.5 (https://startbootstrap.com/theme/business-casual)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
-*/
 // Highlights current date on contact page
 window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
